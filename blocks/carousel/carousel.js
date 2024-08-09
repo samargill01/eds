7a02863cd66afaa96a3c18af2ae52f379b59a44d
Samarpreet Gill <samargill01>
2024-08-09 18:10:14 +0530

    carousel changes

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,10 +1,10 @@
-// import { fetchPlaceholders,getMetadata } from '../../scripts/aem.js';
-// const placeholders = await fetchPlaceholders(getMetadata("locale"));
+import { fetchPlaceholders,getMetadata } from '../../scripts/aem.js';
+const placeholders = await fetchPlaceholders(getMetadata("locale"));
 
-// const { btnNxt,btnPre} = placeholders;
+const { btnNxt = ">",btnPre = "<"} = placeholders;
 export default function decorate(block) {
 
-  // console.log("placeholders ---> ",placeholders,btnNxt,btnPre);
+  console.log("placeholders ---> ",placeholders,btnNxt,btnPre);
     const rows= [...block.children];
     [...block.children].forEach((row,r) => {
       if(r==0){
